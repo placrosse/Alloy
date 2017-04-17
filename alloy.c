f6a21d2c96c5722a6377d45419288cb35e1c4d59
Ian Seyler <ian.seyler@returninfinity.com>
2017-04-17 12:57:07 -0400

    Add typedefs

--- alloy.c
+++ alloy.c
@@ -4,6 +4,16 @@
 /* Global includes */
 #include "libBareMetal.h"
 
+/* Global typedefs */
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long s64;
+
 /* Global variables */
 unsigned short running = 1;
 unsigned char input[256];
