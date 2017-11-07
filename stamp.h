d89f588b542f036f4496026d712419259db68768
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-06 21:12:17 -0500

    Added stamp file

---
+++ stamp.h
@@ -0,0 +1,7 @@
+// =====================================================
+// Alloy -- The shell for the BareMetal operating system
+//
+// Copyright (C) 2017 Return Infinity -- see LICENSE
+//
+// Version 1.0
+// =====================================================
