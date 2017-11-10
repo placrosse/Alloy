340b383db95cdf5cce390e68f066699ec8d352ee
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-10 18:42:07 -0500

    added alloy font header

---
+++ alloy-font.h
@@ -0,0 +1,31 @@
+#ifndef ALLOY_FONT_H
+#define ALLOY_FONT_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+struct alloy_glyph
+{
+	unsigned int ascii_value;
+	unsigned int width;
+	unsigned int height;
+	const unsigned char *buf;
+};
+
+struct alloy_font
+{
+	const struct alloy_glyph *glyph_array;
+	unsigned char glyph_count;
+};
+
+const struct alloy_font alloy_font;
+
+const struct alloy_glyph *alloy_font_get_glyph(const struct alloy_font *font, unsigned int c);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* ALLOY_FONT_H */
