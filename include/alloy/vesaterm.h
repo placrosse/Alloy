a72c55db75a035c81776f6582e747da8957c2ba7
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-10 22:34:15 -0500

    added VESA term header

---
+++ include/alloy/vesaterm.h
@@ -0,0 +1,40 @@
+#ifndef ALLOY_VESATERM_H
+#define ALLOY_VESATERM_H
+
+#include <alloy/term.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/** A terminal implementation using
+ * VESA graphics.
+ */
+
+struct AlloyVesaTerm
+{
+	/** The base structure of the terminal
+	 * implementation.*/
+	struct AlloyTerm base;
+	/** The number of pixels per line. */
+	unsigned int x_res;
+	/** The number of lines in a frame. */
+	unsigned int y_res;
+	/** The number of bits per pixel. */
+	unsigned int depth;
+	/** A poitner to the frame memory contents. */
+	unsigned char *frame_buffer;
+};
+
+/** Initializes the contents of the
+ * terminal structure.
+ */
+
+void alloy_vesaterm_init(struct AlloyVesaTerm *term);
+
+#ifdef __cplusplus
+} /* extern "C" { */
+#endif
+
+#endif /* ALLOY_VESATERM_H */
