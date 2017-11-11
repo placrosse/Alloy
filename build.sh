7600566dbd7678c4fd39baa0f75ed0a429e04611
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-10 20:20:20 -0500

    building alloy font file

--- build.sh
+++ build.sh
@@ -2,30 +2,19 @@
 
 set -e
 
-if [ "$BMFS_INCLUDE_DIR" == "" ]; then
-	echo "BMFS include directory not set."
-	exit 1
-fi
-
-if [ "$BMFS_LIBRARY" == "" ]; then
-	echo "BMFS library not set."
-	exit 1
-fi
-
-if [ "$BAREMETAL_LIBC_INCLUDE_DIR" == "" ]; then
-	echo "BareMetal libc include directory not set."
-	exit 1
-fi
-
-if [ "$BAREMETAL_LIBC_LIBRARY" == "" ]; then
-	echo "BareMetal libc library not set."
-	exit 1
-fi
+cd fonts/source-code-pro
+./build.sh
+cd ../..
 
 CC=gcc
 CFLAGS="-m64 -Wall -Wextra -Werror -Wfatal-errors -g -std=gnu99"
+
+$CC $CFLAGS alloy-font-generator.c -o alloy-font-generator `pkg-config --cflags --libs freetype2`
+
+./alloy-font-generator
+
 CFLAGS="$CFLAGS -nostdlib -nostartfiles -nodefaultlibs -nostdinc"
-CFLAGS="$CFLAGS -fomit-frame-pointer -mno-red-zone"
+CFLAGS="$CFLAGS -fomit-frame-pointer -fno-stack-protector -mno-red-zone"
 CFLAGS="$CFLAGS -I$BMFS_INCLUDE_DIR -I$BAREMETAL_LIBC_INCLUDE_DIR"
 
 LD=ld
@@ -38,5 +27,13 @@ set -u
 $CC $CFLAGS -c alloy.c -o alloy.o
 $OBJCOPY --remove-section .comment alloy.o
 $OBJCOPY --remove-section .eh_frame alloy.o
-$LD $LDFLAGS alloy.o -o alloy.elf -lbmfs -lc
+
+$CC $CFLAGS -c alloy-font.c -o alloy-font.o
+$OBJCOPY --remove-section .comment alloy-font.o
+$OBJCOPY --remove-section .eh_frame alloy-font.o
+
+#$LD $LDFLAGS alloy.o -o alloy.elf -lbmfs -lc
+
+$LD $LDFLAGS alloy.o alloy-font.o -o alloy.elf -lbmfs -lc
+
 $OBJCOPY -O binary alloy.elf alloy.bin
