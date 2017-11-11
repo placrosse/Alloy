bc746d3204c4404999be3ad2abe5c03561de0ef5
Ian Seyler <ian.seyler@returninfinity.com>
2017-11-10 22:38:58 -0500

    Updates

--- loader.c
+++ loader.c
@@ -1,11 +1,9 @@
 /* Alloy Loader - The BareMetal middleware */
 /* Copyright 2017 Return Infinity, Inc */
 
-/* Global includes */
-#include <libBareMetal.h>
-
 /* Global functions */
 unsigned long b_disk_read(void *mem, unsigned long start, unsigned long num, unsigned long disknum);
+int int_strncmp(const char *str1, const char *str2, unsigned int num);
 
 /* Global typedefs */
 typedef unsigned char u8;
@@ -17,11 +15,29 @@ typedef signed short s16;
 typedef signed int s32;
 typedef signed long long s64;
 
+/* Global defines*/
+struct BMFS_Dir
+{
+	char FileName[32];
+	u64 StartingBlock;
+	u64 ReservedBlocks;
+	u64 FileSize;
+	u64 Unused;
+}
+
+/* Global variables */
+unsigned long AlloyStartBlock;
+unsigned long AlloyLength;
+unsigned long MemoryAddress;
+
 int main()
 {
+	MemoryAddress = (void*)0x100000;
 	// Load the BMFS directory into memory
+	b_disk_read(MemoryAddress, 1, 1, 0); // Copy second 4K sector to memory
 	
 	// Does alloy.bin exist?
+	
 	
 	// If so, load it to 0x200000 and jump to it
 	
@@ -32,4 +48,12 @@ unsigned long b_disk_read(void *mem, unsigned long start, unsigned long num, uns
 	unsigned long tlong;
 	asm volatile ("call *0x00100060" : "=c"(tlong) : "a"(start), "c"(num), "d"(disknum), "D"(mem));
 	return tlong;
+}
+
+int int_strncmp(const char *str1, const char *str2, unsigned int num)
+{
+	while(n--)
+		if(*str1++ != *str2++)
+			return *(unsigned char*)(str1-1) - *(unsigned char*)(str2-1)
+	return 0;
 }
