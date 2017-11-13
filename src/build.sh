11e5076d4ac4739851bd9cecceea2851539a7b9d
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-12 19:36:11 -0500

    buidling loader, renamed alloy to alloy.elf

--- src/build.sh
+++ src/build.sh
@@ -19,6 +19,10 @@ LDFLAGS="-static -L $TOP/output/lib -T alloy.ld"
 
 OBJCOPY=objcopy
 
+NASM=nasm
+
+$NASM loader.asm -o loader.bin
+
 $CC $CFLAGS -c alloy.c -o alloy.o
 $OBJCOPY --remove-section .comment alloy.o
 $OBJCOPY --remove-section .eh_frame alloy.o
@@ -30,6 +34,6 @@ $OBJCOPY --remove-section .eh_frame font.o
 $CC $CFLAGS -c term.c -o term.o
 $CC $CFLAGS -c vesaterm.c -o vesaterm.o
 
-$LD $LDFLAGS alloy.o font.o term.o vesaterm.o -o alloy $LDLIBS
+$LD $LDFLAGS alloy.o font.o term.o vesaterm.o -o alloy.elf $LDLIBS
 
 $OBJCOPY -O binary alloy alloy.bin
