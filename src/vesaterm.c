3f8af9612325e8a9cbb881478da4c5aeca510d04
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-14 12:12:41 -0500

    using ascender and descender variables from font face correctly

--- src/vesaterm.c
+++ src/vesaterm.c
@@ -34,7 +34,7 @@ static int vesaterm_write_char(struct AlloyVesaTerm *term, char c)
 
 	/* color in the background */
 
-	for (unsigned int y = 0; (y < alloy_font.line_height) && ((y + term->y_pos) < term->y_res); y++)
+	for (unsigned int y = 0; (y < (alloy_font.ascender - alloy_font.descender)) && ((y + term->y_pos) < term->y_res); y++)
 	{
 		unsigned int background_width = glyph->advance;
 		for (unsigned int x = 0; (x < background_width) && ((x + term->x_pos) < term->x_res); x++)
@@ -56,7 +56,7 @@ static int vesaterm_write_char(struct AlloyVesaTerm *term, char c)
 
 	/* color in the foreground */
 
-	for (unsigned int y = 0; (y < glyph->height) && ((y + term->y_pos + alloy_font.line_height - glyph->top) < term->y_res); y++)
+	for (unsigned int y = 0; (y < glyph->height) && ((y + term->y_pos + alloy_font.ascender - glyph->top) < term->y_res); y++)
 	{
 
 		for (unsigned int x = 0; (x < glyph->width) && ((x + term->x_pos + glyph->left) < term->x_res); x++)
@@ -68,7 +68,7 @@ static int vesaterm_write_char(struct AlloyVesaTerm *term, char c)
 			unsigned int intensity = glyph->buf[glyph_offset];
 
 			unsigned buffer_offset = 0;
-			buffer_offset += (y + term->y_pos + alloy_font.line_height - glyph->top) * term->x_res;
+			buffer_offset += (y + term->y_pos + alloy_font.ascender - glyph->top) * term->x_res;
 			buffer_offset += x + term->x_pos + glyph->left;
 			buffer_offset *= term->depth / 8;
 
