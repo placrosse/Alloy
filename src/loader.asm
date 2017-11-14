a93343fc4cd43dbc6c4200c1dc6d8924d2a81b43
Ian Seyler <ian.seyler@returninfinity.com>
2017-11-14 17:31:06 -0500

    Update loader.asm

--- src/loader.asm
+++ src/loader.asm
@@ -1,5 +1,4 @@
 ; A quick and dirty loader
-; Loads 2MiB from the second block on the disk
 
 
 [BITS 64]
@@ -10,22 +9,35 @@
 start:					; Start of program label
 
 	; Read BMFS directory into memory
-	mov rax, 8
-	mov rcx, 8
+	mov rax, 1
+	mov rcx, 1
 	xor rdx, rdx
 	mov rdi, dir
 	call [b_disk_read]
 
 	; Check directory for "alloy.bin"
+	mov rsi, dir
+checknext:
+	mov al, byte [rsi]
+	cmp al, 0
+	je error
+	mov rdi, alloyname
+	call string_compare
+	jc loadfile
+	add rsi, 64			; next record
+	jmp checknext
 
-	jmp loadfile
-
+error:
 	lea rsi, [rel notfound]		; Load error message
+	mov rcx, 33
 	call [b_output]			; Output the error
 	jmp $				; Halt
 
 loadfile:
 	; Load Alloy to memory
+	add rdi, 32
+	mov rax, [rdi]			; Load the starting block #
+	shl rax, 9			; Quick multiply by 512
 	mov rax, 512			; Start at 2MiB
 	mov rcx, 512			; Load 2MiB
 	xor rdx, rdx
@@ -37,5 +49,51 @@ loadfile:
 
 ret					; Return to OS
 
+; -----------------------------------------------------------------------------
+; string_compare -- See if two strings match
+;  IN:	RSI = string one
+;	RDI = string two
+; OUT:	Carry flag set if same
+string_compare:
+	push rsi
+	push rdi
+	push rbx
+	push rax
+
+string_compare_more:
+	mov al, [rsi]			; Store string contents
+	mov bl, [rdi]
+	test al, al			; End of first string?
+	jz string_compare_terminated
+	cmp al, bl
+	jne string_compare_not_same
+	inc rsi
+	inc rdi
+	jmp string_compare_more
+
+string_compare_not_same:
+	pop rax
+	pop rbx
+	pop rdi
+	pop rsi
+	clc
+	ret
+
+string_compare_terminated:
+	test bl, bl			; End of second string?
+	jnz string_compare_not_same
+
+	pop rax
+	pop rbx
+	pop rdi
+	pop rsi
+	stc
+	ret
+; -----------------------------------------------------------------------------
+
+
+alloyname: db 'alloy.bin', 0
 notfound: db 'Binary not found. System halted.', 13, 0
+
+align 16
 dir:
