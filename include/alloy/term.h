912a87fd56b94ba77551628d36e4be75639e1e65
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-11 17:54:49 -0400

    added new callback structure for term

--- include/alloy/term.h
+++ include/alloy/term.h
@@ -6,66 +6,94 @@ extern "C"
 {
 #endif
 
-struct AlloyTerm
+struct AlloyTermData;
+
+struct AlloyCursorPos
 {
-	/** Used for passing data to the
-	 * term implementation. */
-	void *data;
-	/** Called when the term is no longer
-	 * going to be used by the caller. */
-	void (*done)(void *data);
-	/** Called to clear the screen and
-	 * reset the cursor position. */
-	int (*clear)(void *data);
-	/** Gets the current cursor position. */
-	int (*get_cursor)(void *data, unsigned int *line, unsigned int *column);
-	/** Gets the width and height, in character spaces, of the terminal. */
-	int (*get_size)(void *data, unsigned int *width, unsigned int *height);
-	/** Writes text to the screen. */
-	int (*write)(void *data, const char *str, unsigned int str_len);
-	/** Sets the background color. */
-	int (*set_background)(void *data, unsigned long int color);
-	/** Sets the cursor position. */
-	int (*set_cursor)(void *data, unsigned int line, unsigned int column);
-	/** Sets the foreground color. */
-	int (*set_foreground)(void *data, unsigned long int color);
-	/** Toggles the cursor visibility. */
-	int (*toggle_cursor)(void *data);
+	unsigned int line;
+	unsigned int column;
 };
 
-void alloy_term_init(struct AlloyTerm *term);
+struct AlloyTermSize
+{
+	unsigned int width;
+	unsigned int height;
+};
 
-void alloy_term_done(struct AlloyTerm *term);
+struct AlloyColor
+{
+	unsigned char red;
+	unsigned char green;
+	unsigned char blue;
+};
 
-int alloy_term_clear(struct AlloyTerm *term);
+struct AlloyTerm
+{
+	struct AlloyTermData *(*init)(void);
+	/** Called when the term is no longer
+	 * going to be used by the caller. */
+	void (*done)(struct AlloyTermData *term_data);
+	/** Called to clear the screen and
+	 * reset the cursor position. */
+	int (*clear)(struct AlloyTermData *term_data);
+	/** Gets the current cursor position. */
+	int (*get_cursor)(struct AlloyTermData *term_data,
+	                  struct AlloyCursorPos *cursor_pos);
+	/** Gets the width and height, in character spaces, of the terminal. */
+	int (*get_size)(struct AlloyTermData *term_data,
+	                struct AlloyTermSize *term_size);
+	/** Writes text to the screen. */
+	int (*write)(struct AlloyTermData *term_data,
+	             const char *str, unsigned int str_len);
+	/** Sets the background color. */
+	int (*set_background)(struct AlloyTermData *term_data,
+	                      const struct AlloyColor *background_color);
+	/** Sets the cursor position. */
+	int (*set_cursor)(struct AlloyTermData *term_data,
+	                  const struct AlloyCursorPos *cursor_pos);
+	/** Sets the foreground color. */
+	int (*set_foreground)(struct AlloyTermData *term_data,
+	                      const struct AlloyColor *foreground);
+};
 
-int alloy_term_clear_line(struct AlloyTerm *term);
+struct AlloyTermData *alloy_term_init(const struct AlloyTerm *term);
 
-int alloy_term_get_cursor(struct AlloyTerm *term,
-                        unsigned int *line,
-                        unsigned int *column);
+void alloy_term_done(const struct AlloyTerm *term,
+                     struct AlloyTermData *term_data);
 
-int alloy_term_get_size(struct AlloyTerm *term,
-                        unsigned int *width,
-                        unsigned int *height);
+int alloy_term_clear(const struct AlloyTerm *term,
+                     struct AlloyTermData *term_data);
 
-int alloy_term_toggle_cursor_visibility(struct AlloyTerm *term);
+int alloy_term_clear_line(const struct AlloyTerm *term,
+                          struct AlloyTermData *term_data);
 
-int alloy_term_set_background(struct AlloyTerm *term,
-                              unsigned long int color);
+int alloy_term_get_cursor(const struct AlloyTerm *term,
+                          struct AlloyTermData *term_data,
+                          struct AlloyCursorPos *cursor_pos);
 
-int alloy_term_set_cursor(struct AlloyTerm *term,
-                          unsigned int line,
-                          unsigned int column);
+int alloy_term_get_size(const struct AlloyTerm *term,
+                        struct AlloyTermData *term_data,
+                        struct AlloyTermSize *term_size);
 
-int alloy_term_set_foreground(struct AlloyTerm *term,
-                              unsigned long int color);
+int alloy_term_set_background(const struct AlloyTerm *term,
+                              struct AlloyTermData *term_data,
+                              const struct AlloyColor *color);
 
-int alloy_term_write(struct AlloyTerm *term,
+int alloy_term_set_cursor(const struct AlloyTerm *term,
+                          struct AlloyTermData *term_data,
+                          const struct AlloyCursorPos *pos);
+
+int alloy_term_set_foreground(const struct AlloyTerm *term,
+                              struct AlloyTermData *term_data,
+                              const struct AlloyColor *color);
+
+int alloy_term_write(const struct AlloyTerm *term,
+                     struct AlloyTermData *term_data,
                      const char *str,
                      unsigned int str_len);
 
-int alloy_term_write_asciiz(struct AlloyTerm *term,
+int alloy_term_write_asciiz(const struct AlloyTerm *term,
+                            struct AlloyTermData *term_data,
                             const char *str);
 
 #ifdef __cplusplus
--- src/term.c
+++ src/term.c
@@ -1,137 +1,144 @@
 #include <alloy/term.h>
 
-#include <errno.h>
-#include <stdlib.h>
-#include <string.h>
+#include <alloy/errno.h>
+#include <alloy/types.h>
 
-void alloy_term_init(struct AlloyTerm *term)
+struct AlloyTermData *alloy_term_init(const struct AlloyTerm *term)
 {
-	term->data = NULL;
-	term->done = NULL;
-	term->clear = NULL;
-	term->get_cursor = NULL;
-	term->get_size = NULL;
-	term->toggle_cursor = NULL;
-	term->set_background = NULL;
-	term->set_cursor = NULL;
-	term->set_foreground = NULL;
-	term->write = NULL;
+	if (term->init == ALLOY_NULL)
+		return ALLOY_NULL;
+	else
+		return term->init();
 }
 
-void alloy_term_done(struct AlloyTerm *term)
+void alloy_term_done(const struct AlloyTerm *term,
+                     struct AlloyTermData *term_data)
 {
-	if (term->done != NULL)
-		term->done(term->data);
+	if (term->done != ALLOY_NULL)
+		term->done(term_data);
 }
 
-int alloy_term_clear(struct AlloyTerm *term)
+int alloy_term_clear(const struct AlloyTerm *term,
+                     struct AlloyTermData *term_data)
 {
-	if (term->clear == NULL)
-		return -EFAULT;
-
-	return term->clear(term->data);
+	if (term->clear == ALLOY_NULL)
+		return ALLOY_ENOSYS;
+	else
+		return term->clear(term_data);
 }
 
-int alloy_term_clear_line(struct AlloyTerm *term)
+int alloy_term_clear_line(const struct AlloyTerm *term,
+                          struct AlloyTermData *term_data)
 {
-	unsigned int width = 0;
-	unsigned int height = 0;
+	struct AlloyTermSize term_size;
 
-	int err = alloy_term_get_size(term, &width, &height);
+	int err = alloy_term_get_size(term, term_data, &term_size);
 	if (err != 0)
 		return err;
 
-	unsigned int original_line = 0;
-	unsigned int original_column = 0;
+	unsigned int width = term_size.width;
 
-	err = alloy_term_get_cursor(term, &original_line, &original_column);
+	struct AlloyCursorPos original_pos;
+
+	err = alloy_term_get_cursor(term, term_data, &original_pos);
 	if (err != 0)
 		return err;
 
-	err = alloy_term_set_cursor(term, original_line, 1);
+	unsigned int original_line = original_pos.line;
+
+	struct AlloyCursorPos start_pos;
+	start_pos.line = original_line;
+	start_pos.column = 1;
+
+	err = alloy_term_set_cursor(term, term_data, &start_pos);
 	if (err != 0)
 		return err;
 
 	for (unsigned int i = 0; i < width; i++)
-		alloy_term_write(term, " ", 1);
+		alloy_term_write(term, term_data, " ", 1);
 
-	err = alloy_term_set_cursor(term, original_line, 1);
+	original_pos.column = 1;
+
+	err = alloy_term_set_cursor(term, term_data, &original_pos);
 	if (err != 0)
 		return err;
 
 	return 0;
 }
 
-int alloy_term_get_cursor(struct AlloyTerm *term,
-                          unsigned int *line,
-                          unsigned int *column)
+int alloy_term_get_cursor(const struct AlloyTerm *term,
+                          struct AlloyTermData *term_data,
+                          struct AlloyCursorPos *cursor_pos)
 {
-	if (term->get_cursor == NULL)
-		return -1;
-
-	return term->get_cursor(term->data, line, column);
+	if (term->get_cursor == ALLOY_NULL)
+		return ALLOY_ENOSYS;
+	else
+		return term->get_cursor(term_data, cursor_pos);
 }
 
-int alloy_term_get_size(struct AlloyTerm *term,
-                        unsigned int *width,
-                        unsigned int *height)
+int alloy_term_get_size(const struct AlloyTerm *term,
+                        struct AlloyTermData *term_data,
+                        struct AlloyTermSize *term_size)
 {
-	if (term->get_size == NULL)
-		return -1;
-
-	return term->get_size(term->data, width, height);
+	if (term->get_size == ALLOY_NULL)
+		return ALLOY_ENOSYS;
+	else
+		return term->get_size(term_data, term_size);
 }
 
-int alloy_term_toggle_cursor(struct AlloyTerm *term)
+int alloy_term_set_background(const struct AlloyTerm *term,
+                              struct AlloyTermData *term_data,
+                              const struct AlloyColor *color)
 {
-	if (term->toggle_cursor == NULL)
-		return -EFAULT;
-
-	return term->toggle_cursor(term->data);
+	if (term->set_background == ALLOY_NULL)
+		return ALLOY_ENOSYS;
+	else
+		return term->set_background(term_data, color);
 }
 
-int alloy_term_set_background(struct AlloyTerm *term,
-                              unsigned long int color)
+int alloy_term_set_cursor(const struct AlloyTerm *term,
+                          struct AlloyTermData *term_data,
+                          const struct AlloyCursorPos *pos)
 {
-	if (term->set_background == NULL)
-		return -EFAULT;
-
-	return term->set_background(term->data, color);
+	if (term->set_cursor == ALLOY_NULL)
+		return ALLOY_ENOSYS;
+	else
+		return term->set_cursor(term_data, pos);
 }
 
-int alloy_term_set_cursor(struct AlloyTerm *term,
-                          unsigned int line,
-                          unsigned int column)
+int alloy_term_set_foreground(const struct AlloyTerm *term,
+                              struct AlloyTermData *term_data,
+                              const struct AlloyColor *color)
 {
-	if (term->set_cursor == NULL)
-		return -EFAULT;
-
-	return term->set_cursor(term->data, line, column);
+	if (term->set_foreground == ALLOY_NULL)
+		return ALLOY_ENOSYS;
+	else
+		return term->set_foreground(term_data, color);
 }
 
-int alloy_term_set_foreground(struct AlloyTerm *term,
-                              unsigned long int color)
-{
-	if (term->set_foreground == NULL)
-		return -EFAULT;
-
-	return term->set_foreground(term->data, color);
-}
-
-int alloy_term_write(struct AlloyTerm *term,
+int alloy_term_write(const struct AlloyTerm *term,
+                     struct AlloyTermData *term_data,
                      const char *str,
                      unsigned int str_len)
 {
-	if (term->write != NULL)
-		term->write(term->data, str, str_len);
-
-	return 0;
+	if (term->write == ALLOY_NULL)
+		return ALLOY_ENOSYS;
+	else
+		return term->write(term_data, str, str_len);
 }
 
-int alloy_term_write_asciiz(struct AlloyTerm *term,
+int alloy_term_write_asciiz(const struct AlloyTerm *term,
+                            struct AlloyTermData *term_data,
                             const char *str)
 {
-	unsigned int str_len = strlen(str);
+	while (*str)
+	{
+		int err = alloy_term_write(term, term_data, str, 1);
+		if (err != 0)
+			return err;
 
-	return alloy_term_write(term, str, str_len);
+		str++;
+	}
+
+	return 0;
 }
