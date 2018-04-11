80eb152c9700765e77d9ca441fb4b531fe35f592
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-11 17:56:44 -0400

    added default config

---
+++ make/default-config.mk
@@ -0,0 +1,14 @@
+ifndef TOP
+$(error "The TOP variable should be defined.")
+endif
+DESTDIR ?=
+PREFIX ?= /opt/return-infinity
+PLATFORM ?= linux
+PLATFORM_$(PLATFORM) = 1
+CROSS_COMPILE ?=
+CFLAGS += -Wall -Wextra -Werror -Wfatal-errors
+CFLAGS += -I $(TOP)/include
+CC := $(CROSS_COMPILE)gcc
+AR := $(CROSS_COMPILE)ar
+AS := $(CROSS_COMPILE)as
+LD := $(CROSS_COMPILE)gcc
