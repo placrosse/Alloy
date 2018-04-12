83e583d58bd8e0cac9f79f8336680a7a25fbede7
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-11 21:33:19 -0400

    added shell wrapper functions

--- include/alloy/shell.h
+++ include/alloy/shell.h
@@ -2,12 +2,14 @@
 #define ALLOY_SHELL_H
 
 #include <alloy/input.h>
+#include <alloy/types.h>
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+struct AlloyColor;
 struct AlloyTerm;
 struct AlloyTermData;
 
@@ -28,6 +30,28 @@ struct AlloyShell
 void alloy_shell_init(struct AlloyShell *shell);
 
 void alloy_shell_done(struct AlloyShell *shell);
+
+int alloy_shell_clear(struct AlloyShell *shell);
+
+int alloy_shell_clear_line(struct AlloyShell *shell);
+
+int alloy_shell_get_char(struct AlloyShell *shell,
+                         alloy_utf8 *c);
+
+int alloy_shell_get_line(struct AlloyShell *shell);
+
+int alloy_shell_set_foreground(struct AlloyShell *shell,
+                               const struct AlloyColor *color);
+
+void alloy_shell_set_term(struct AlloyShell *shell,
+                          const struct AlloyTerm *term);
+
+int alloy_shell_write(struct AlloyShell *shell,
+                      const char *str,
+                      alloy_size str_size);
+
+int alloy_shell_write_asciiz(struct AlloyShell *shell,
+                             const char *str);
 
 #ifdef __cplusplus
 } /* extern "C" */
--- src/shell.c
+++ src/shell.c
@@ -1,7 +1,23 @@
 #include <alloy/shell.h>
 
+#include <alloy/errno.h>
 #include <alloy/term.h>
 #include <alloy/types.h>
+
+static int init_term(struct AlloyShell *shell)
+{
+	if (shell->term == ALLOY_NULL)
+		return ALLOY_ENOSYS;
+
+	if (shell->term_data != ALLOY_NULL)
+		return 0;
+
+	shell->term_data = alloy_term_init(shell->term);
+	if (shell->term_data == ALLOY_NULL)
+		return ALLOY_ENOMEM;
+
+	return 0;
+}
 
 void alloy_shell_init(struct AlloyShell *shell)
 {
@@ -18,4 +34,132 @@ void alloy_shell_done(struct AlloyShell *shell)
 		shell->term = ALLOY_NULL;
 		shell->term_data = ALLOY_NULL;
 	}
+}
+
+int alloy_shell_clear(struct AlloyShell *shell)
+{
+	int err = init_term(shell);
+	if (err != 0)
+		return err;
+
+	err = alloy_term_clear(shell->term, shell->term_data);
+	if (err != 0)
+		return err;
+
+	return 0;
+}
+
+int alloy_shell_clear_line(struct AlloyShell *shell)
+{
+	int err = init_term(shell);
+	if (err != 0)
+		return err;
+
+	err = alloy_term_clear_line(shell->term, shell->term_data);
+	if (err != 0)
+		return err;
+
+	return 0;
+}
+
+int alloy_shell_get_char(struct AlloyShell *shell,
+                         alloy_utf8 *c)
+{
+	int err = init_term(shell);
+	if (err != 0)
+		return err;
+
+	err = alloy_term_get_char(shell->term, shell->term_data, c);
+	if (err != 0)
+		return err;
+
+	return 0;
+}
+
+int alloy_shell_get_line(struct AlloyShell *shell)
+{
+	for (;;)
+	{
+		alloy_utf8 c = 0;
+
+		int err = alloy_shell_get_char(shell, &c);
+		if (err != 0)
+			break;
+
+		/* enter key */
+		if (c == 0x1c)
+			break;
+
+		/* backspace key */
+		if (c == 0x0e)
+		{
+			alloy_input_backspace(&shell->input);
+			alloy_shell_clear_line(shell);
+			alloy_shell_write_asciiz(shell, "> ");
+			alloy_shell_write(shell, shell->input.buf, shell->input.buf_len);
+			continue;
+		}
+
+		alloy_shell_write(shell, &c, 1);
+
+		alloy_input_insert(&shell->input, c);
+	}
+
+	return 0;
+}
+
+int alloy_shell_set_foreground(struct AlloyShell *shell,
+                               const struct AlloyColor *color)
+{
+	int err = init_term(shell);
+	if (err != 0)
+		return err;
+
+	err = alloy_term_set_foreground(shell->term, shell->term_data, color);
+	if (err != 0)
+		return err;
+
+	return 0;
+}
+
+void alloy_shell_set_term(struct AlloyShell *shell,
+                          const struct AlloyTerm *term)
+{
+	if (shell->term != ALLOY_NULL)
+	{
+		alloy_term_done(shell->term, shell->term_data);
+	}
+
+	shell->term = term;
+	shell->term_data = ALLOY_NULL;
+}
+
+int alloy_shell_write(struct AlloyShell *shell,
+                      const char *str,
+                      alloy_size str_size)
+{
+
+	int err = init_term(shell);
+	if (err != 0)
+		return err;
+
+	err = alloy_term_write(shell->term, shell->term_data, str, str_size);
+	if (err != 0)
+		return err;
+
+	return 0;
+}
+
+int alloy_shell_write_asciiz(struct AlloyShell *shell,
+                             const char *str)
+{
+	int err = init_term(shell);
+	if (err != 0)
+		return err;
+
+	err = alloy_term_write_asciiz(shell->term, shell->term_data, str);
+	if (err != 0)
+		return err;
+
+	return 0;
 }
