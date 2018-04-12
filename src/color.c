e9735bfd21effb69de1c1f0c3b87058a065fa14b
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-11 21:21:56 -0400

    added color structure

---
+++ src/color.c
@@ -0,0 +1,38 @@
+/* ===============================================================
+ * Alloy - A platform-independent terminal and shell program.
+ * Copyright (C) 2017 - 2018 Return Infinity
+ * See LICENSE for license information.
+ * ===============================================================
+ */
+
+#include <alloy/color.h>
+
+const struct AlloyColor alloy_red = {
+	0xff,
+	0x00,
+	0x00
+};
+
+const struct AlloyColor alloy_green = {
+	0x00,
+	0xff,
+	0x00
+};
+
+const struct AlloyColor alloy_blue = {
+	0x00,
+	0x00,
+	0xff
+};
+
+const struct AlloyColor alloy_white = {
+	0xff,
+	0xff,
+	0xff
+};
+
+const struct AlloyColor alloy_black = {
+	0x00,
+	0x00,
+	0x00
+};
