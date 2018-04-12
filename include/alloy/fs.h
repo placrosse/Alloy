4df92c35a627a7af2109e3f801addad68138ce67
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-12 17:02:01 -0400

    started file system header

---
+++ include/alloy/fs.h
@@ -0,0 +1,25 @@
+/* ===============================================================
+ * Alloy - A platform-independent terminal and shell program.
+ * Copyright (C) 2017 - 2018 Return Infinity
+ * See LICENSE for license information.
+ * ===============================================================
+ */
+
+#ifndef ALLOY_FS_H
+#define ALLOY_FS_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+struct AlloyFS
+{
+	/* TODO : */
+};
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* ALLOY_FS_H */
