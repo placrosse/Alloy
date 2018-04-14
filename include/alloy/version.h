3a28ae02be345534e5d5138d754f3e62143e1e94
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-14 11:57:05 -0400

    added version header

---
+++ include/alloy/version.h
@@ -0,0 +1,15 @@
+/* ===============================================================
+ * Alloy - A platform-independent terminal and shell program.
+ * Copyright (C) 2017 - 2018 Return Infinity
+ * See LICENSE for license information.
+ * ===============================================================
+ */
+
+#ifndef ALLOY_VERSION_H
+#define ALLOY_VERSION_H
+
+#define ALLOY_VERSION 0x000200
+
+#define ALLOY_VERSION_STRING "0.2.0"
+
+#endif /* ALLOY_VERSION_H */
