3dc94ccd36347b88b22eee7dc32d0338fb4a2526
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-14 11:14:03 -0400

    input structure uses heap now

--- lib/input.c
+++ lib/input.c
@@ -1,10 +1,7 @@
 #include <alloy/input.h>
 
-#include <alloy/error.h>
-
-#ifndef NULL
-#define NULL ((void *) 0x00)
-#endif
+#include <alloy/errno.h>
+#include <alloy/heap.h>
 
 static int calculate_x_bias(struct AlloyInput *input)
 {
@@ -40,11 +37,42 @@ static int calculate_x_bias(struct AlloyInput *input)
 
 void alloy_input_init(struct AlloyInput *input)
 {
-	input->buf = NULL;
+	input->heap = ALLOY_NULL;
+	input->buf = ALLOY_NULL;
 	input->buf_len = 0;
 	input->buf_pos = 0;
 	input->buf_res = 0;
 	input->x_bias = 0;
+}
+
+void alloy_input_done(struct AlloyInput *input)
+{
+	if (input->heap == ALLOY_NULL)
+		return;
+
+	alloy_heap_free(input->heap, input->buf);
+
+	input->buf = ALLOY_NULL;
+	input->buf_len = 0;
+	input->buf_pos = 0;
+	input->buf_res = 0;
+	input->x_bias = 0;
+}
+
+void alloy_input_set_heap(struct AlloyInput *input,
+                          struct AlloyHeap *heap)
+{
+	/* Make sure that the current heap, if present,
+	 * does not get overwritten. This would mess up
+	 * reallocations of the input buffer. */
+	if (input->heap != ALLOY_NULL)
+		return;
+
+	/* Make sure a heap was actually passed. */
+	if (heap == ALLOY_NULL)
+		return;
+
+	input->heap = heap;
 }
 
 void alloy_input_clear(struct AlloyInput *input)
@@ -61,6 +89,30 @@ void alloy_input_set_tab_width(struct AlloyInput *input,
                                unsigned int tab_width)
 {
 	input->tab_width = tab_width;
+}
+
+int alloy_input_reserve(struct AlloyInput *input,
+                        alloy_size res)
+{
+	if (input->heap == ALLOY_NULL)
+		return ALLOY_EFAULT;
+
+	char *buf = input->buf;
+
+	buf = alloy_heap_realloc(input->heap, buf, res);
+	if (buf == ALLOY_NULL)
+		return ALLOY_ENOMEM;
+
+	input->buf = buf;
+	input->buf_res = res;
+
+	if (input->buf_len > res)
+		input->buf_len = res;
+
+	if (input->buf_pos > res)
+		input->buf_pos = res;
+
+	return 0;
 }
 
 int alloy_input_backspace(struct AlloyInput *input)
@@ -83,9 +135,16 @@ int alloy_input_backspace(struct AlloyInput *input)
 int alloy_input_insert(struct AlloyInput *input, char c)
 {
 	if (input->buf_pos >= input->buf_res)
-		return ALLOY_ERROR_BUFFER_FULL;
-	else if ((c < ' ') && (c > '~') && (c != '\t'))
-		return ALLOY_ERROR_UNKNOWN_CHARACTER;
+	{
+		alloy_size new_size = ((input->buf_pos + 63) / 64) * 64;
+
+		int err = alloy_input_reserve(input, new_size);
+		if (err != 0)
+			return err;
+	}
+
+	if ((c < ' ') && (c > '~') && (c != '\t'))
+		return ALLOY_EINVAL;
 
 	for (unsigned int i = input->buf_len; i > input->buf_pos; i--)
 		input->buf[i] = input->buf[i - 1];
@@ -106,7 +165,6 @@ int alloy_input_insert(struct AlloyInput *input, char c)
 int alloy_input_left(struct AlloyInput *input)
 {
 	if (input->buf_pos <= 0)
-		/* TODO : Should this error? */
 		return 0;
 
 	input->buf_pos--;
@@ -117,7 +175,6 @@ int alloy_input_left(struct AlloyInput *input)
 int alloy_input_right(struct AlloyInput *input)
 {
 	if (input->buf_pos >= input->buf_len)
-		/* TODO : Should this error? */
 		return 0;
 
 	input->buf_pos++;
