88a1452c7bfcf3c11612a5b90d92ed708311db20
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-14 08:30:58 -0400

    implemented command parsing

--- lib/cmd.c
+++ lib/cmd.c
@@ -7,6 +7,58 @@
 
 #include <alloy/cmd.h>
 
+#include <alloy/errno.h>
+#include <alloy/heap.h>
+
+static int push_arg(struct AlloyCmd *cmd,
+                    const char *arg,
+                    alloy_size arglen)
+{
+	if (cmd->heap == ALLOY_NULL)
+		return ALLOY_EFAULT;
+
+	alloy_size argc = cmd->argc + 1;
+
+	char **argv = cmd->argv;
+
+	argv = alloy_heap_realloc(cmd->heap, argv, (argc + 1) * sizeof(char *));
+	if (argv == ALLOY_NULL)
+		return ALLOY_ENOMEM;
+
+	cmd->argv = argv;
+
+	char *new_arg = alloy_heap_malloc(cmd->heap, arglen + 1);
+	if (new_arg == ALLOY_NULL)
+		return ALLOY_ENOMEM;
+
+	for (alloy_size i = 0; i < arglen; i++)
+		new_arg[i] = arg[i];
+
+	new_arg[arglen] = 0;
+
+	argv[argc - 1] = new_arg;
+
+	argv[argc] = ALLOY_NULL;
+
+	cmd->argc++;
+
+	return 0;
+}
+
+static void free_args(struct AlloyCmd *cmd)
+{
+	if (cmd->heap == ALLOY_NULL)
+		return;
+
+	for (alloy_size i = 0; i < cmd->argc; i++)
+		alloy_heap_free(cmd->heap, &cmd->argv[i]);
+
+	alloy_heap_free(cmd->heap, cmd->argv);
+
+	cmd->argv = ALLOY_NULL;
+	cmd->argc = 0;
+}
+
 enum AlloyCmdID alloy_cmd_id_parse(const char *cmd)
 {
 	if ((cmd[0] == 'c')
@@ -70,11 +122,71 @@ enum AlloyCmdID alloy_cmd_id_parse(const char *cmd)
 void alloy_cmd_init(struct AlloyCmd *cmd)
 {
 	cmd->id = ALLOY_CMD_UNKNOWN;
+	cmd->argv = ALLOY_NULL;
+	cmd->argc = 0;
+}
+
+void alloy_cmd_free(struct AlloyCmd *cmd)
+{
+	free_args(cmd);
+}
+
+void alloy_cmd_set_heap(struct AlloyCmd *cmd,
+                        struct AlloyHeap *heap)
+{
+	if (heap != ALLOY_NULL)
+		cmd->heap = heap;
 }
 
 int alloy_cmd_parse(struct AlloyCmd *cmd, const char *line)
 {
-	(void) cmd;
-	(void) line;
+	if (cmd->heap == ALLOY_NULL)
+		return ALLOY_EFAULT;
+
+	free_args(cmd);
+
+	alloy_size argpos = 0;
+	alloy_size arglen = 0;
+	alloy_size i = 0;
+
+	while (line[i] != 0)
+	{
+		if ((line[i] == ' ')
+		 || (line[i] == '\t')
+		 || (line[i] == '\n'))
+		{
+			if (arglen == 0)
+			{
+				i++;
+				continue;
+			}
+
+			int err = push_arg(cmd, &line[argpos], arglen);
+			if (err != 0)
+				return err;
+
+			arglen = 0;
+		}
+		else
+		{
+			if (arglen == 0)
+				argpos = i;
+
+			arglen++;
+		}
+
+		i++;
+	}
+
+	if (arglen > 0)
+	{
+		int err = push_arg(cmd, &line[argpos], arglen);
+		if (err != 0)
+			return err;
+	}
+
+	if (cmd->argc > 0)
+		cmd->id = alloy_cmd_id_parse(cmd->argv[0]);
+
 	return 0;
 }
