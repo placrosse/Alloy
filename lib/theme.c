162eda90695adf3465991d155cd0bd95c737b6d4
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-14 12:49:34 -0400

    added molokai theme

--- lib/theme.c
+++ lib/theme.c
@@ -7,13 +7,29 @@
 
 #include <alloy/theme.h>
 
+/* The default theme is based on this molokai scheme:
+ *  - 0x272822
+ *  - 0xf92672
+ *  - 0x66d9ef
+ *  - 0xa6e22e
+ *  - 0xfd971f
+ * */
+
 void alloy_theme_init(struct AlloyTheme *theme)
 {
-	theme->cmd_builtin.red = 0xe4;
-	theme->cmd_builtin.green = 0xff;
-	theme->cmd_builtin.blue = 0xdc;
+	theme->cmd_builtin.red = 0xf9;
+	theme->cmd_builtin.green = 0x26;
+	theme->cmd_builtin.blue = 0x72;
 
-	theme->cmd_external.red = 0xc0;
-	theme->cmd_external.green = 0xe9;
-	theme->cmd_external.blue = 0xc8;
+	theme->cmd_external.red = 0xf9;
+	theme->cmd_external.green = 0x26;
+	theme->cmd_external.blue = 0x72;
+
+	theme->normal_foreground.red = 0xff;
+	theme->normal_foreground.green = 0xff;
+	theme->normal_foreground.blue = 0xff;
+
+	theme->normal_background.red = 0x27;
+	theme->normal_background.green = 0x28;
+	theme->normal_background.blue = 0x22;
 }
