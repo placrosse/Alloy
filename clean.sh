425a8f99df2ebba4a26adbedc4eb79b4295a3b8b
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-14 20:08:17 -0400

    added missing clean scripts

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd fonts/source-code-pro
+./clean.sh
+cd ..
+
+cd utils
+./clean.sh
+cd ..
+
+cd lib
+./clean.sh
+cd ..
+
+cd src
+./clean.sh
+cd ..
---
+++ lib/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+CC=gcc
+CFLAGS="${CFLAGS} -Wall -Wextra -Werror -Wfatal-errors"
+CFLAGS="${CFLAGS} -fno-stack-protector"
+CFLAGS="${CFLAGS} -fomit-frame-pointer"
+CFLAGS="${CFLAGS} -mno-red-zone"
+CFLAGS="${CFLAGS} -I ../include"
+
+$CC $CFLAGS -c cmd.c
+$CC $CFLAGS -c color.c
+$CC $CFLAGS -c heap.c
+$CC $CFLAGS -c host.c
+$CC $CFLAGS -c input.c
+$CC $CFLAGS -c scanner.c
+$CC $CFLAGS -c scheme.c
+$CC $CFLAGS -c shell.c
+$CC $CFLAGS -c term.c
+
+AR=ar
+ARFLAGS=rcs
+
+$AR $ARFLAGS liballoy.a cmd.o color.o heap.o host.o input.o scanner.o scheme.o shell.o term.o
