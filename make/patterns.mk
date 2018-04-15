ae68a7c854d33609f8f8465949da0a728ea43177
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-15 13:54:27 -0400

    moved Alloy loader to separate project

--- make/patterns.mk
+++ make/patterns.mk
@@ -6,18 +6,10 @@
 	@echo "CC      $@"
 	$(CC) $(CFLAGS) -c $< -o $@
 
-%.o: %.asm
-	@echo "NASM    $@"
-	$(NASM) $< -f elf64 -o $@
-
 %.a:
 	@echo "AR      $@"
 	$(AR) $(ARFLAGS) $@ $^
 
 %: %.o
-	@echo "LD      $@"
-	$(LD) $(LDFLAGS) $^ -o $@ $(LDLIBS)
-
-%.elf: %.o
 	@echo "LD      $@"
 	$(LD) $(LDFLAGS) $^ -o $@ $(LDLIBS)
